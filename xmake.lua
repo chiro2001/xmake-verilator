5ffb9f424db8c379c1428865acd3d425faa1bb26
Chiro Liang <Chiro2001@163.com>
2022-09-09 17:10:37 +0800

    init

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,12 @@
-task("verilator")
+task("verilate")
     set_category("plugin")
     on_run(function ()
-        import("kconfig")
-        kconfig.clean()
+        import("kconfig", { rootdir = path.join(os.projectdir(), "plugins/xmake-kconfig") })
+        print(kconfig)
+        -- kconfig.clean()
     end)
+    set_menu {
+                usage = "xmake verilate",
+                description = "Verilate targets",
+                options = {}
+            }
